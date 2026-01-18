acc5c211295b561e12f4e766020031d04a09ce8b
Davi Torres <73488542+davift@users.noreply.github.com>
2026-01-18 09:13:59 -0500

    Update u24-pave.sh

--- u24-pave.sh
+++ u24-pave.sh
@@ -3,6 +3,7 @@ sudo apt update && sudo apt install cloudstack-management -y
 sudo mysql -e "CREATE DATABASE cloud;"
 sudo mysql -e "CREATE DATABASE cloud_usage;"
 sudo mysql -e "FLUSH PRIVILEGES;"
+sudo sed -i "s/^127\.0\.1\.1.*/$(hostname -I | awk '{print $1}') acs.local acs/" /etc/hosts
 sudo cloudstack-setup-databases cloud:dft.wiki@localhost --schema-only && sudo cloudstack-setup-management
 sleep 5
 sudo tail -f /var/log/cloudstack/management/management-server.log
